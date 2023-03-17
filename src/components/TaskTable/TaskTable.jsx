ac1a80b9284e6cef17e6c9243a7015898b2f3e9e
Erich Engler <erich.engler@hotmail.com>
2023-03-16 23:09:39 -0500

    dealing with a lot of proxy errors whenever I turn the server on or off. going to ask about this in class next week. GET and POST are working though.

--- src/components/TaskTable/TaskTable.jsx
+++ src/components/TaskTable/TaskTable.jsx
@@ -43,9 +43,9 @@ function TaskTable () {
                 <h2>Create a Task</h2>
                 <label htmlFor="task-input">Task:</label>
                 <input id="task-input" onChange={e => setTaskName(e.target.value)} />
-                {/* <br />
+                <br />
                 <label htmlFor="task-input">Completed:</label>
-                <input id="completed-input" value="No" onChange={e => setTaskCompleted(e.target.value)} /> */}
+                <input id="completed-input" onChange={e => setTaskCompleted(e.target.value)} />
                 <br />
                 <button id="createTaskButton">Create</button>
             </form>
